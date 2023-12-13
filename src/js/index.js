ffaa6e180adb05ac234f3cef0d0f4bef4b352a65
VasylPopaz <pafos1997@gmail.com>
2023-12-13 16:12:11 +0100

    finish task

--- src/js/index.js
+++ src/js/index.js
@@ -6,58 +6,30 @@ import { createMarkup } from './create-markup';
 
 const searchForm = document.querySelector('.js-search-form');
 const gallery = document.querySelector('.gallery');
-const targetElem = document.querySelector('.js-target');
-let lightbox = {};
-let page = 1;
-let totalPages = 0;
-let query = '';
 
+let lightbox = new SimpleLightbox('.gallery a');
+let page = 1;
+let query = '';
 let observer = new IntersectionObserver(loadMore, {
+  root: null,
   rootMargin: '470px',
   threshold: 1.0,
 });
 
 searchForm.addEventListener('submit', handleSubmit);
 
-async function handleSubmit(event) {
-  event.preventDefault();
-  observer.unobserve(targetElem);
-  gallery.innerHTML = '';
-  page = 1;
-  query = event.target.searchQuery.value;
-
-  try {
-    const photos = await getPhotos(query, page);
-    if (!photos.hits.length)
-      return Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-
-    Notify.success(`Hurray! We found ${photos.totalHits} images.`);
-    totalPages = Math.ceil(photos.totalHits / 40);
-    const markup = createMarkup(photos.hits);
-    gallery.insertAdjacentHTML('beforeend', markup);
-    lightbox = new SimpleLightbox('.gallery a');
-    observer.observe(targetElem);
-    changeStatusObserver();
-  } catch (error) {
-    Notify.failure('Sorry, something went wrong. Please try again.');
-    console.log(error.name);
-    console.log(error.message);
-  }
-}
-
 function loadMore(entries) {
-  changeStatusObserver();
-
   entries.forEach(async entry => {
     if (entry.isIntersecting) {
+      observer.unobserve(entry.target);
+      page += 1;
+
       try {
-        page += 1;
         const photos = await getPhotos(query, page);
         const markup = createMarkup(photos.hits);
         gallery.insertAdjacentHTML('beforeend', markup);
         lightbox.refresh();
+        checkMorePhotos(photos.totalHits);
 
         const viewportHeight = window.innerHeight;
         setTimeout(() => {
@@ -75,9 +47,36 @@ function loadMore(entries) {
   });
 }
 
-function changeStatusObserver() {
-  if (page === totalPages) {
-    observer.unobserve(targetElem);
+async function handleSubmit(event) {
+  event.preventDefault();
+  gallery.innerHTML = '';
+  page = 1;
+  query = event.target.searchQuery.value;
+
+  try {
+    const photos = await getPhotos(query, page);
+    if (!photos.hits.length)
+      return Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+
+    Notify.success(`Hurray! We found ${photos.totalHits} images.`);
+    const markup = createMarkup(photos.hits);
+    gallery.insertAdjacentHTML('beforeend', markup);
+    lightbox.refresh();
+    checkMorePhotos(photos.totalHits);
+  } catch (error) {
+    Notify.failure('Sorry, something went wrong. Please try again.');
+    console.log(error.name);
+    console.log(error.message);
+  }
+}
+
+function checkMorePhotos(total) {
+  if (page < Math.ceil(total / 40)) {
+    const item = document.querySelector('.gallery__item:last-child');
+    observer.observe(item);
+  } else {
     Notify.info(`We're sorry, but you've reached the end of search results.`);
   }
 }
